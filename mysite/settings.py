cbfcf4ee80ec42fb41ed0a41fb4dbba533085aa6
Nirmal Vekariya <n9879970008@gmail.com>
2021-05-30 12:48:41 +0530

    Init

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,7 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/Users/HP/PycharmProjects/webProject1/mysite/project/static/',
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
