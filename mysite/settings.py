27f4115ad493e457762c71cd1b70e27970b418a3
Hind Farousi <hindfarousi@gmail.com>
2021-06-04 02:50:20 +0300

    media root set

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,6 +139,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
 django_heroku.settings(locals())
--- project/models.py
+++ project/models.py
@@ -19,7 +19,7 @@ class Article(models.Model):
     title = models.CharField(max_length=250)
     body = models.TextField()
     date = models.DateField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='media')
     likes = models.ManyToManyField(User)
     published = models.BooleanField(default=True)
 
@@ -49,7 +49,7 @@ class nasaNew(models.Model):
     title = models.CharField(max_length=250)
     body = models.TextField()
     date = models.DateField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='media')
     published = models.BooleanField(default=True)
 
     def summary(self):
@@ -63,7 +63,7 @@ class nasaNew(models.Model):
 class FilesAdmin(models.Model):
     adminupload=models.FileField(upload_to='media')
     title=models.CharField(max_length=200)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='media')
     date = models.DateField()
 
     def __str__(self):
@@ -89,7 +89,7 @@ def save(self, *args, **kwargs):
 #Topic model
 class Topic(models.Model):
     title=models.CharField(max_length=200)
-    pdfupload=models.FileField(upload_to='pdf/')
+    pdfupload=models.FileField(upload_to='media')
     time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -99,7 +99,7 @@ class Topic(models.Model):
 #Book model
 class Book(models.Model):
     title=models.CharField(max_length=200)
-    image=models.FileField(upload_to='books/')
+    image=models.FileField(upload_to='media')
     link1 = models.CharField(max_length=200)
     link2 = models.CharField(max_length=200, blank=True)
     link3=models.CharField(max_length=200, blank=True)
