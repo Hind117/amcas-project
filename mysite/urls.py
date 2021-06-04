1c0550284d0c42c466ad05588f5eb3cfd30d146b
Hind Farousi <hindfarousi@gmail.com>
2021-06-04 21:21:14 +0300

    urls altered

--- mysite/urls.py
+++ mysite/urls.py
@@ -41,7 +41,7 @@ path('RHLHRclubadmin/', admin.site.urls),
                   path('', include('project.urls')),
 
                   
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
