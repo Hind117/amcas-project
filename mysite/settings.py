c2a8f7659a05c4ed5635decaeec2fea1a0c37032
Hind Farousi <hindfarousi@gmail.com>
2021-06-04 21:13:45 +0300

    STATICFILES_DIRS added

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,10 +134,11 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 MEDIA_URL = '/media/'
-MEDIA_ROOT  = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
